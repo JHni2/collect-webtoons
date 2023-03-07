46eacee7f5128c57a28f9e4b9a03f4d7d506b014
Jihyun <wlgus0260@naver.com>
2023-03-07 20:27:09 +0900

    기타 css 수정

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -56,7 +56,7 @@ export default function InputSearch(): JSX.Element {
           <ul className="hidden top-[105px] left-0 rounded-b sm:block sm:!absolute sm:top-[51px] w-full max-h-96 shadow-md text-base-content overflow-y-auto bg-[#e5e7eb] text-[#777]">
             {searchedWebtoons.map((webtoon) => {
               return (
-                <li className="text-sm bg-white hover:bg-[#eef1f5] transition-all" key={webtoon.webtoonId}>
+                <li className="text-sm bg-white hover:bg-[#eef1f5] hover:text-[#333] transition-all" key={webtoon.webtoonId}>
                   <Link className="flex items-center gap-2 p-[12px_16px]" to={webtoon.url}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-quote shrink-0" viewBox="0 0 19 19">
                       <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />

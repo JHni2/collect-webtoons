00f1cce849a99a86687643e5404bc5a1d1ffcb97
Jihyun <wlgus0260@naver.com>
2023-03-07 17:44:35 +0900

    검색 css 수정

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -15,7 +15,7 @@ export default function Search(): JSX.Element {
   return (
     <>
       <Loading />
-      <section ref={$section} className="relative m-[0_15px]" id="container">
+      <section ref={$section} className="relative m-[0_15px] transition-all" id="container">
         <SearchPage />
       </section>
     </>

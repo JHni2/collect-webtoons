be7b77be172817620d1f0342fe0cf45a7b28ac59
Jihyun <wlgus0260@naver.com>
2023-09-20 16:12:48 +0900

    fix: more버튼 수정

--- src/components/WebtoonDetailPage.tsx
+++ src/components/WebtoonDetailPage.tsx
@@ -1,87 +1,96 @@
-import { doc, updateDoc } from 'firebase/firestore'
-import { db } from '../firebase'
-import QueryString from 'qs'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import React, { useContext, useEffect, useState } from 'react'
-import { IWebtoon, IWebtoon2 } from '../stores/Webtoon/types'
-import { UserInfoContext } from '../context/UserInfoContext'
-import { useRecoilValueLoadable } from 'recoil'
-import { webtoonsList } from '../stores/Webtoon/webtoons'
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import QueryString from 'qs';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { IWebtoon, IWebtoon2 } from '../stores/Webtoon/types';
+import { UserInfoContext } from '../context/UserInfoContext';
+import { useRecoilValueLoadable } from 'recoil';
+import { webtoonsList } from '../stores/Webtoon/webtoons';
 
 export default function WebtoonDetailPage(): JSX.Element {
-  const location = useLocation()
-  const searchQuery = QueryString.parse(location.search, { ignoreQueryPrefix: true })
-  const { user } = useContext(UserInfoContext)
-  const [heart, setheart] = useState(false)
-  const [moreBtn, setMoreBtn] = useState(false)
-  const test: any = []
-  const navigate = useNavigate()
-  const WebtoonsLoadable = useRecoilValueLoadable(webtoonsList)
-  let webtoons: IWebtoon2[] = 'hasValue' === WebtoonsLoadable.state ? WebtoonsLoadable.contents.documents : []
+  const location = useLocation();
+  const searchQuery = QueryString.parse(location.search, { ignoreQueryPrefix: true });
+  const { user } = useContext(UserInfoContext);
+  const [heart, setheart] = useState(false);
+  const [moreBtn, setMoreBtn] = useState(false);
+  const test: any = [];
+  const navigate = useNavigate();
+  const WebtoonsLoadable = useRecoilValueLoadable(webtoonsList);
+  let webtoons: IWebtoon2[] = 'hasValue' === WebtoonsLoadable.state ? WebtoonsLoadable.contents.documents : [];
 
-  webtoons = webtoons.filter((item) => Object.values(item.fields.webtoonId)[0] === searchQuery.titleID)
+  webtoons = webtoons.filter((item) => Object.values(item.fields.webtoonId)[0] === searchQuery.titleID);
 
-  const [favWebtoons, setFavWebtoons] = useState<IWebtoon[]>([...user.wishList])
+  const [favWebtoons, setFavWebtoons] = useState<IWebtoon[]>([...user.wishList]);
 
   const addToFavoriteHandler = async (webtoon: any) => {
     if (user.nickname.length === 0) {
-      navigate('/login')
-      return
+      navigate('/login');
+      return;
     } else {
-      const favoriteRef = doc(db, 'user', user.nickname)
+      const favoriteRef = doc(db, 'user', user.nickname);
       if (favWebtoons.length === 0) {
-        setFavWebtoons([...favWebtoons, webtoon[0]])
-        setheart(true)
+        setFavWebtoons([...favWebtoons, webtoon[0]]);
+        setheart(true);
         await updateDoc(favoriteRef, {
           wishList: [...favWebtoons, webtoon[0]],
-        })
+        });
       }
       favWebtoons.forEach(async (favWebtoon, idx) => {
         if (favWebtoon.webtoonId === webtoon[0].webtoonId) {
-          favWebtoons.splice(idx, 1)
-          setFavWebtoons(favWebtoons)
-          setheart(false)
+          favWebtoons.splice(idx, 1);
+          setFavWebtoons(favWebtoons);
+          setheart(false);
           await updateDoc(favoriteRef, {
             wishList: favWebtoons,
-          })
+          });
         } else {
-          setFavWebtoons([...favWebtoons, webtoon[0]])
-          setheart(true)
+          setFavWebtoons([...favWebtoons, webtoon[0]]);
+          setheart(true);
           await updateDoc(favoriteRef, {
             wishList: [...favWebtoons, webtoon[0]],
-          })
+          });
         }
-      })
+      });
     }
-  }
+  };
 
   useEffect(() => {
     if (document.getElementById('des') !== null) {
-      document.getElementById('des')?.offsetHeight !== document.getElementById('des')?.scrollHeight && setMoreBtn(true)
+      document.getElementById('des')?.offsetHeight !== document.getElementById('des')?.scrollHeight && setMoreBtn(true);
     }
     user.wishList &&
       webtoons.length > 0 &&
       user.wishList.forEach((item) => {
-        if (item.title === Object.values(webtoons[0].fields.title)[0]) setheart(true)
-      })
-  }, [webtoons[0]])
+        if (item.title === Object.values(webtoons[0].fields.title)[0]) setheart(true);
+      });
+  }, [webtoons[0]]);
 
-  const [more, setMore] = useState(false)
+  const [more, setMore] = useState(false);
   const handleMoreBtn = () => {
-    setMore(!more)
-  }
+    setMore(!more);
+  };
 
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
-  const url = webtoons.length > 0 && (innerWidth < 480 ? `${Object.values(webtoons[0].fields.url)[0].split('https://').join('https://m.')}` : Object.values(webtoons[0].fields.url)[0])
-  const delay = 300
-  let timer: any
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const url = webtoons.length > 0 && (innerWidth < 480 ? `${Object.values(webtoons[0].fields.url)[0].split('https://').join('https://m.')}` : Object.values(webtoons[0].fields.url)[0]);
+  const delay = 300;
+  let timer: any;
 
   window.addEventListener('resize', function () {
-    clearTimeout(timer)
+    clearTimeout(timer);
     timer = setTimeout(function () {
-      setInnerWidth(window.innerWidth)
-    }, delay)
-  })
+      setInnerWidth(window.innerWidth);
+    }, delay);
+  });
+
+  const desSize = document.getElementById('des');
+  useEffect(() => {
+    if (desSize?.clientHeight === desSize?.scrollHeight) {
+      setMoreBtn(false);
+    } else {
+      setMoreBtn(true);
+    }
+  }, [innerWidth]);
 
   return (
     <div>
@@ -104,7 +113,7 @@ export default function WebtoonDetailPage(): JSX.Element {
                     day: Object.values(webtoons[0].fields.day)[0],
                     des: Object.values(webtoons[0].fields.des)[0],
                     genre: webtoons[0].fields.genre.arrayValue.values.map((genre) => {
-                      return Object.values(genre)[0]
+                      return Object.values(genre)[0];
                     }),
                     img: Object.values(webtoons[0].fields.img)[0],
                     searchKeyword: Object.values(webtoons[0].fields.searchKeyword)[0],
@@ -149,7 +158,7 @@ export default function WebtoonDetailPage(): JSX.Element {
                   <p key={idx} onClick={() => navigate(`/?genre=${Object.values(genre)[0]}`)} className="inline-block text-sm border rounded-full p-[2px_6px] text-zinc-400 border-zinc-400 cursor-pointer">
                     {Object.values<string>(genre)[0]}
                   </p>
-                )
+                );
               })}
             </div>
             <div>
@@ -168,5 +177,5 @@ export default function WebtoonDetailPage(): JSX.Element {
         </div>
       )}
     </div>
-  )
+  );
 }
